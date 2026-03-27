a58d6f7f5f3b700d98b8cfbbf80b6eca891c77a6
Karan Pandit <karanpanditvlogs2468@gmail.com>
2026-03-28 05:07:30 +0530

    music library, to play , change it accordingly 2 u

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "stay" : "https://youtu.be/iDEi7HtnJfg?si=FqLf41tloARPst4-",
+    "khyaal" : "https://youtu.be/JFUoE0ArWd4?si=cwnwbw6EQL87k6Lv",
+    "dopamine" : "https://youtu.be/iOgR7hi90Ac?si=0r8lkSOMFBAKDoPF",
+    "haseen" : "https://youtu.be/IltsOcCj1Ak?si=joH-x45dXmzmblGQ"
+}
+
+# Add the music accordingly which you like !
